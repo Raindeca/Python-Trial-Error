28159ec4ef86a00c831cdf68bf97eaaa8db0b93a
Raindeca Dzulikrom Haqqu <preserved10core@gmail.com>
2020-03-09 20:45:27 +0700

    added minor fix

--- ClinicTrialError/__services.py
+++ ClinicTrialError/__services.py
@@ -1,5 +1,6 @@
 class Services:
-    
+    service_payment = []
+
     def __init__(self, drug, room, duration, maternity_services):
         self.drug = drug
         self.room = room
@@ -8,29 +9,40 @@ class Services:
     
     def get_room(self):
         if self.room == "Class 1":
+            service_payment.append(self.duration * 450000)
             return self.duration * 450000
         elif self.room == "Class 2":
+            service_payment.append(self.duration * 250000)
             return self.duration * 250000
         elif self.room == "Class 3":
+            service_payment.append(self.duration * 150000)
             return self.duration * 150000
         elif self.room == "VIP":
+            service_payment.append(self.duration * 600000)
             return self.duration * 600000
         elif self.room == "VVIP":
+            service_payment.append(self.duration * 800000)
             return self.duration * 800000
         else:
+            service_payment.append(0)
             return 0
     
     def get_maternity_servives(self):
         if self.maternity_services == "normal":
+            service_payment.append(4000000)
             return 4000000
         else:
+            service_payment.append(8000000)
             return 8000000
     
     def medical_action(self):
         if self.maternity_services == "normal":
+            service_payment.append(3000000 + 1500000)
             return 3000000 + 1500000
         else:
+            service_payment.append(8000000)
             return 8000000
     
     def get_drug(self):
+        service_payment.append(self.drug)
         return self.drug
