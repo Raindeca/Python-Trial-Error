c83c6f71fd52f63b13391e713960b5e1a369f666
Raindeca Dzulikrom Haqqu <preserved10core@gmail.com>
2020-03-11 22:48:01 +0700

    added instance object

--- ClinicTrialError/__invoice.py
+++ ClinicTrialError/__invoice.py
@@ -10,30 +10,26 @@ class Invoice:
         self.person = person
         self.payment = payment
 
-    total = sum(Services.service_payment)
     
     def invoice(self):
-        
-        # person = Tasks.fetch_patient()
-        # payment = Tasks.fetch_service()
-        # total = sum(Services.service_payment) 
+         
 
         print("\n\n\nBiodata Pasien:")
-        print("\t- Nama Pasien: ", Patients(self.person).get_full_name())
-        print("\t- Nama Wali: ", Patients(self.person).get_guardian_name())
-        print("\t- Alamat: ", Patients(self.person).get_address())
-        print("\t- No. Telepon: ", Patients(self.person).get_phone())
-        print("\t- Gol. Darah: ", Patients(self.person).get_blood_type())
+        print("\t- Nama Pasien: ", self.person.get_full_name())
+        print("\t- Nama Wali: ", self.person.get_guardian_name())
+        print("\t- Alamat: ", self.person.get_address())
+        print("\t- No. Telepon: ", self.person.get_phone())
+        print("\t- Gol. Darah: ", self.person.get_blood_type())
         print("Pembayaran:\n")
         
         print(
             tabulate(
                 [
-                    [Services.get_room(), 
-                    Services.medical_action(), 
-                    Services.get_maternity_services(), 
-                    Services.get_drug(), 
-                    total]
+                    [self.payment.get_room(), 
+                    self.payment.medical_action(), 
+                    self.payment.get_maternity_services(), 
+                    self.payment.get_drug(), 
+                    self.payment.get_service_payment()]
                 ],
 
                 headers= [
